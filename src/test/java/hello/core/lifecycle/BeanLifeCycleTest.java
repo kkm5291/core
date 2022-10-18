f80471f0ac06a62688f009f2aa7940335e54f46d
kkm5291 <kkm23125291@gmail.com>
2022-10-18 14:17:28 +0900

    bean callback lifecycle

---
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,32 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.lang.annotation.Annotation;
+
+public class BeanLifeCycleTest {
+
+
+    @Test
+    public void lifeCycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient(); // 생성자에서 필요한 정보만 간단하게 받은 후
+            networkClient.setUrl("http://hello-spring.dev"); // 초기화 단계에서 url 설정해주기
+            return networkClient;
+        }
+    }
+}
